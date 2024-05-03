57edb031ac8a72b8b2a7aac91bb40de2b86bfa75
EmilioSZ <emiliosz1708@gmail.com>
2024-05-03 16:31:46 -0500

    Navbar and slider

--- script.js
+++ script.js
@@ -12,3 +12,41 @@ var swiper = new Swiper(".mySwiper", {
         stretch: 0
     }
 });
+
+function App() {}
+
+window.onload = function(event) {
+    var app = new App();
+    window.app = app;
+}
+
+App.prototype.processingButton = function(event) {
+    const btn = event.currentTarget;
+    const carruselList = event.currentTarget.parentNode;
+    const track = event.currentTarget.parentNode.querySelector("#track");
+    const carrusel = track.querySelectorAll(".carrusel");
+
+    const carruselWidth = carrusel[0].offsetWidth;
+    const trackWidth = track.offsetWidth;
+    const listWidth = carruselList.offsetWidth;
+
+    let leftPosition = track.style.left === "" ? 0 : parseFloat(track.style.left);
+
+    if (btn.dataset.button === "button-prev") {
+        prevAction(leftPosition, carruselWidth, track);
+    } else {
+        nextAction(leftPosition, trackWidth, listWidth, carruselWidth, track);
+    }
+}
+
+let prevAction = (leftPosition, carruselWidth, track) => {
+    if (leftPosition < 0) {
+        track.style.left = `${leftPosition + carruselWidth}px`;
+    }
+}
+
+let nextAction = (leftPosition, trackWidth, listWidth, carruselWidth, track) => {
+    if (leftPosition > (listWidth - trackWidth)) {
+        track.style.left = `${leftPosition - carruselWidth}px`;
+    }
+}
